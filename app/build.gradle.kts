41f8c071d0a3b6e0d148b801b85afbd6cf29a62c
José Antonio <77889590+Tony4772@users.noreply.github.com>
2026-09-11 21:24:03 -0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ebyzom.ebyzomparkingfinder"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
